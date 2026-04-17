fc1f9da9d192ebf484f064b5c6f1709c36fe7c5c
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-17 19:55:25 -0300

    Delete auth_routes.py

--- auth_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
-
-@auth_router.get("/")
-
-async def auth():
-    """This is the auth route, you can implement your authentication logic here."""
-    return {"message": "you've accessed the auth routes"}
