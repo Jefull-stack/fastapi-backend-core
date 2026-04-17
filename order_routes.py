e5af233eaf50aadd4691a24a5a0bb0cc050fabe3
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-17 19:54:54 -0300

    Delete order_routes.py

--- order_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-order_router = APIRouter(prefix="/orders", tags=["orders"])
-
-@order_router.get("/")
-
-async def get_orders():
-    """This is the orders route, you can implement your order handling logic here."""
-    return {"message": "you've accessed the orders routes"}
