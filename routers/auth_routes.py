324a617812fe16a9758f059460919a4995336518
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-18 19:22:38 -0300

    style(auth): format code for better readability in signup and login functions

--- routers/auth_routes.py
+++ routers/auth_routes.py
@@ -9,44 +9,42 @@ from schemas import UserCreate, UserResponse, UserLogin
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-@auth_router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-def signup(payload: UserCreate, db: Session = Depends(take_session)):
-    
-   email = payload.email.lower().strip()
 
-   new_user = User(
-    name=payload.name,
-    email=email,
-    hashed_password=hash_password(payload.password)
+@auth_router.post(
+    "/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
+def signup(payload: UserCreate, db: Session = Depends(take_session)):
+
+    email = payload.email.lower().strip()
+
+    new_user = User(
+        name=payload.name, email=email, hashed_password=hash_password(payload.password)
     )
-   try:
+    try:
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
-   except IntegrityError:
+    except IntegrityError:
         db.rollback()
-        raise HTTPException(
-            status_code=400,
-            detail="Email already registered"
-        )
-   return new_user
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return new_user
+
 
 @auth_router.post("/login")
 def login(payload: UserLogin, db: Session = Depends(take_session)):
-    
+
     user = db.query(User).filter(User.email == payload.email.lower().strip()).first()
 
     if not user:
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    
+
     valid, new_hash = verify_and_update(payload.password, user.hashed_password)
 
     if not valid:
-     raise HTTPException(status_code=401, detail="Invalid credentials")
-    
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
     if new_hash:
-       user.hashed_password = new_hash
-       db.commit()
+        user.hashed_password = new_hash
+        db.commit()
     token = create_access_token({"sub": user.id})
     return {"access_token": token, "token_type": "bearer"}
-    
