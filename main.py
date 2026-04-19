7377338c0f465b4d07245bba52d41d43a740b128
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-19 19:35:18 -0300

    fix(main): reorder initialization to ensure database tables are created before app starts

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from database.database import Base, engine
+from models import User, Order, OrderStatus
 from routers.auth_routes import auth_router
 from routers.order_routes import order_router
 
-app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
+app = FastAPI()
 app.include_router(auth_router)
 app.include_router(order_router)
