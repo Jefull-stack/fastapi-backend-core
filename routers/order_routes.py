cabf487666d4b3885928ecd2b5690946be932c25
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-19 19:07:52 -0300

    feat(orders): implement get_orders and create_order endpoints

--- routers/order_routes.py
+++ routers/order_routes.py
@@ -1,9 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import take_session
+from schemas import OrderCreate, OrderResponse
+from models import Order
+
 
 order_router = APIRouter(prefix="/orders", tags=["orders"])
 
 @order_router.get("/")
+def get_orders(session: Session = Depends(take_session)):
+    orders = session.query(Order).all()
+    return orders
 
-async def get_orders():
-    """This is the orders route, you can implement your order handling logic here."""
-    return {"message": "you've accessed the orders routes"}
+
+@order_router.post("/")
+async def create_order(payload: OrderCreate, session: Session = Depends(take_session)):
+    new_order = Order(user_id=payload.user_id)
+    session.add(new_order)
+    session.commit()
+    session.refresh(new_order)
+    return {"message": f"New order created with success. Order ID: {new_order.id}"}
+    
