d11f4e9924191bc88d177e939373edc7683a4fe4
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-19 18:08:09 -0300

    fix(config): set default value for SECRET_KEY in Settings class

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
+    SECRET_KEY: str = "CHANGE_ME"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
