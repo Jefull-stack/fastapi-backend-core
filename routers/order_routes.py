52e5d3c373b9ac8e8f5069d32478aa57b9b4c963
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-20 00:22:16 -0300

    feat(orders): protect GET and POST endpoints with JWT authentication

--- routers/order_routes.py
+++ routers/order_routes.py
@@ -1,21 +1,33 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from dependencies.auth import take_session
-from schemas import OrderCreate, OrderResponse
-from models import Order
+from dependencies.auth import get_current_user
+from dependencies.session import take_session
+from schemas import OrderCreate
+from models import User, Order
 
 
 order_router = APIRouter(prefix="/orders", tags=["orders"])
 
 @order_router.get("/")
-def get_orders(session: Session = Depends(take_session)):
+def get_orders(
+    session: Session = Depends(take_session), 
+    current_user: User = Depends(get_current_user)
+    ):
     orders = session.query(Order).all()
     return orders
 
-
 @order_router.post("/")
-async def create_order(payload: OrderCreate, session: Session = Depends(take_session)):
-    new_order = Order(user_id=payload.user_id)
+async def create_order(
+    payload: OrderCreate,
+    session: Session = Depends(take_session),
+    current_user: User = Depends(get_current_user)
+    ):
+    new_order = Order(
+        user_id=payload.user_id,
+        item_name=payload.item_name,
+        quantity=payload.quantity,
+        price=payload.price
+    )
     session.add(new_order)
     session.commit()
     session.refresh(new_order)
