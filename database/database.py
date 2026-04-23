040cf5dd36f5292291d52b355e935bb6f98cf53c
Jefull-stack <aaraojefgermano5851@gmail.com>
2026-04-23 11:16:13 -0300

    refactor(database): update DATABASE_URL to use PostgreSQL and improve formatting

--- database/database.py
+++ database/database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "postgresql://user:password@localhost:5432/dbname"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine)
 
 Base = declarative_base()
